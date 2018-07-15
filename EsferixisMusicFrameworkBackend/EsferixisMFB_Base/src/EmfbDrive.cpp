7ce1c5314ce1fe068a80a669873e3e224e82f814
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-07-14 22:48:19 -0300

    Prueba exitosa con guitarra

---
+++ EsferixisMusicFrameworkBackend/EsferixisMFB_Base/src/EmfbDrive.cpp
@@ -0,0 +1,13 @@
+#include "EmfbDrive.h"
+
+EMFB_API float EmfbDrive(float k, EmfbFloats *frames) {
+	float a = sin(  )
+
+	for (size_t i = 0; i < frames.size; i++) {
+		
+
+		float x = frames[i];
+
+
+	}
+}
