58c4e5ca1152182c9c86b382406b63e86308de37
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-08-09 19:53:23 -0300

    Primera implementación del productor de señales

--- Framework/Esferixis/MusicFramework/Signal/Producer.hs
+++ Framework/Esferixis/MusicFramework/Signal/Producer.hs
@@ -1,17 +1,13 @@
 module Esferixis.MusicFramework.Signal.Producer
-   ( ProducerState(ProducerState)
-   , StatefulProducer(StatefulProducer) ) where
+   ( ProducerState(ProducerState, psMaxPopChunkLength, psPopChunk, psCloseSourceStream) ) where
 
 import Data.Word
 import Control.Concurrent.Async
 import Data.Maybe
+import Control.Concurrent.Async
 
-data ProducerState dataChunk = ProducerState { psMaxPopChunkLength :: Word32 -- Máxima longitud de datos que se puede extraer
-                                             , psPopChunk :: Word32 -> IO (Maybe (Async dataChunk), Maybe (ProducerState dataChunk)) -- Recibe la longitud deseada y devuelve potencialmente un 'chunk' de datos y la sección siguiente (Si no termina el stream)
-                                             , psCloseSourceStream :: IO () -- Cierra el stream de origen
+data ProducerState dataChunk = ProducerState { psMaxPopChunkLength :: Word64 -- Máxima longitud de datos que se puede extraer
+                                             , psPopChunk :: Word64 -> IO (Maybe (Async dataChunk), Maybe (ProducerState dataChunk)) -- Recibe la longitud deseada y devuelve potencialmente un 'chunk' de datos y la sección siguiente (Si no termina el stream)
+                                             , psCloseSourceStream :: IO (Async ()) -- Cierra el stream de origen
                                              }
 
-data StatefulProducer dataChunk = StatefulProducer { spActionsByDuration :: [(Word32, IO ())] -- Acciones por duración
-                                                   , spTick :: Word32 -> IO dataChunk -- Produce un chunk de datos de la longitud especificada
-                                                   , spClose :: IO () -- Cierra el productor
-                                                   }
