6314da97a6c51a1fa6ec49333d8561f989e2ab95
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-08-13 00:27:40 -0300

    Definición recursiva de productor y de transformador de señales

--- Framework/Esferixis/MusicFramework/Signal/Producer.hs
+++ Framework/Esferixis/MusicFramework/Signal/Producer.hs
@@ -1,15 +1,19 @@
 module Esferixis.MusicFramework.Signal.Producer
-   ( ProducerState(ProducerState, psMaxPopChunkLength, psPopChunk) ) where
+   ( ProducerState(ProducerState, psMaxChunkLength, psPopChunk) ) where
 
 import Data.Word
 import Data.Maybe
 import Esferixis.MusicFramework.Signal
 
 {- 
-   Representación abstracta de lo que es un estado de productor
+   Representación abstracta de lo que es un estado de productor.
    Representa un estado del productor que se obtendrá efectivamente
-   en un tiempo indeterminado en el futuro
+   en un tiempo indeterminado en el futuro.
+
+   Tipos
+
+   sd: Signal Data
 -}
-data ProducerState sd = ProducerState { psMaxPopChunkLength :: Word64 -- Máxima longitud de datos que se puede extraer
-                                      , psPopChunk :: Word64 -> (SignalChunk sd, Maybe (ProducerState sd)) -- Recibe la longitud deseada y devuelve un 'chunk' de señal y la sección siguiente (Si no termina el stream). Cuando termina el stream devuelve un chunk de señal de longitud cero, indicando que termina el stream.
+data ProducerState sd = ProducerState { psMaxChunkLength :: Word64 -- Máxima longitud de datos que se puede extraer
+                                      , psPopChunk :: Word64 -> (SignalChunk sd, ProducerState sd) -- Recibe la longitud deseada y devuelve un 'chunk' de señal y la sección siguiente (Si no termina el stream). Cuando termina el stream devuelve un chunk de señal de longitud cero, indicando que termina el stream.
                                       }
--- Framework/Esferixis/MusicFramework/Signal/Transformer.hs
+++ Framework/Esferixis/MusicFramework/Signal/Transformer.hs
@@ -4,12 +4,36 @@ module Esferixis.MusicFramework.Signal.Transformer
 import Data.Word
 import Data.Maybe
 import Esferixis.MusicFramework.Signal
+import Esferixis.MusicFramework.Signal.Producer
 
 {- 
-   Representación abstracta de lo que es un transformador
+   Representación abstracta de lo que es un transformador.
    Representa un estado del transformador que se obtendrá efectivamente
-   en un tiempo indeterminado en el futuro
+   en un tiempo indeterminado en el futuro.
+
+   Tipos
+
+   isd: Input Signal Data
+   osd: Output Signal Data
 -}
-data TransformerState sd = TransformerState { tsMaxChunkLength :: Word64 -- Máxima longitud de datos que se puede transformar
-                                            , tsTransform :: SignalChunk sd -> (SignalChunk sd, Maybe (TransformerState sd)) -- Recibe la longitud deseada, devuelve un 'chunk' de señal y la sección siguiente (Si no termina el stream)
-                                            }
+data TransformerState isd osd = TransformerState { tsMaxChunkLength :: Word64 -- Máxima longitud de datos que se puede transformar
+                                                 , tsTransform :: SignalChunk isd -> (SignalChunk osd, TransformerState isd osd) -- Recibe el chunk de señal de entrada, devuelve el chunk de salida y el siguiente estado del transformador. Cuando recibe un chunk de señal de longitud cero, significa que se termina el stream de entrada.
+                                                 }
+
+{-
+    Dado el estado de productor y el estado de transformador
+    especificados, devuelve un estado de productor compuesto
+
+    Tipos
+
+    isd: Input Signal Data
+    osd: Output Signal Data
+-}
+(>>>) :: ProducerState isd -> TransformerState isd osd -> ProducerState osd
+(>>>) producerState transformerState =
+   ProducerState { psMaxChunkLength = min ( psMaxChunkLength producerState ) ( tsMaxChunkLength transformerState )
+                 , psPopChunk = \requestedChunkLength ->
+                    let (inputChunk, nextInputProducerState) = psPopChunk producerState requestedChunkLength
+                        (outputChunk, nextTransformerState) = tsTransform transformerState inputChunk
+                    in (outputChunk, nextInputProducerState >>> nextTransformerState)
+                 }
