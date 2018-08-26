ea46d41b3d86694d711aa633b56de5072ed72967
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-08-25 21:00:15 -0300

    Completado de promesa imperativa, con bloqueo de thread pendiente

--- Framework/Esferixis/Mutable/Promise.hs
+++ Framework/Esferixis/Mutable/Promise.hs
@@ -1,30 +1,71 @@
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
-module Esferixis.Mutable.Promise where
+module Esferixis.Mutable.Promise(Promise, Future, newPromise, pSet, future, fGet) where
 
 import Data.Word
 import Data.Maybe
 import Control.Concurrent.Lock
 import Data.Mutable
 import Data.IORef
+import Control.Exception
 
 data Promise a =
    Promise {
-        pPendingActions :: BDeque RealWorld (IO ())
+        pNotifyActions :: BDeque RealWorld (IO a -> IO())
       , pValue :: IORef (Maybe (IO a))
       , pLock :: Lock
       }
 
 data Future a = Future (Promise a)
-fGetPromise (Future promise) = promise
 
 newPromise :: IO (Promise a)
 newPromise = do
-   pendingActions <- newColl
-   value <- newIORef Nothing
+   notifyActions <- newColl
+   valueRef <- newIORef Nothing
    lock <- new
    return ( Promise {
-        pPendingActions = pendingActions
-      , pValue = value
+        pNotifyActions = notifyActions
+      , pValue = valueRef
       , pLock = lock
       } )
+
+pSet :: Promise a -> IO a -> IO ()
+pSet promise ioaction = do
+   result <- (try ioaction) :: IO (Either SomeException _)
+   let getAction = 
+          case result of
+             Left e -> throwIO e
+             Right value -> return value
+
+   with (pLock promise) $ do
+      let valueRef = pValue promise
+      value <- readIORef valueRef
+      case value of
+         Nothing -> writeIORef valueRef (Just getAction)
+         Just oldValue -> fail "Cannot set value when it has been set"
+
+   pNotifyValue (pNotifyActions promise) getAction
+
+future :: Promise a -> Future a
+future promise = Future promise
+
+fGet :: Future a -> (IO a -> IO()) -> IO ()
+fGet (Future promise) callback = do
+   nextAction <- with (pLock promise) $ do
+      value_opt <- readIORef (pValue promise)
+      case value_opt of
+         Just value -> return (callback value)
+         Nothing -> do
+            pushFront (pNotifyActions promise) callback
+            return (return ())
+
+   nextAction
+
+pNotifyValue :: BDeque RealWorld (IO a -> IO()) -> IO a -> IO ()
+pNotifyValue deque getValue = do
+   notifyAction_opt <- popBack deque
+   case notifyAction_opt of
+      Just notifyAction -> do
+         notifyAction getValue
+         pNotifyValue deque getValue
+      Nothing -> return ()
