4305513917e97176d1018f23ac4d5c5a2d4866c4
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-09-08 21:18:19 -0300

    Correcciónde nombre de constructor de estado del productor stateful

--- Framework/Esferixis/MusicFramework/Signal/Stateful/Producer.hs
+++ Framework/Esferixis/MusicFramework/Signal/Stateful/Producer.hs
@@ -15,7 +15,7 @@ import Esferixis.MusicFramework.Signal.Stateful.SignalChunk
    Representación abstracta de un productor stateful
    no manejado
 -}
-data SFProducerSt m sc = UnmanagedProducerSt {
+data SFProducerSt m sc = SFProducerSt {
      sfpRemainingFrames :: Maybe Word64 -- Devuelve la cantidad de frames que quedan sin producir, si es infinito devuelve Nothing
    , sfpTick :: (Monad m, SFSignalChunk m sc) => SignalChunkSection sc -> m ( SFProducerSt m sc ) -- Realiza un 'tick', produce una sección de señal en la sección de chunk especificada y devuelve el próximo estado
    , sfpDelete :: (Monad m) => m () -- Destruye el productor
