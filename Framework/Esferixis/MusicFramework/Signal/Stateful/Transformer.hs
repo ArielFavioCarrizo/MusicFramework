7ca825350489c5450390af49c1a5e6ab58ab7d37
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-09-08 21:00:55 -0300

    Avances en transformador stateful para secciones stateless

--- Framework/Esferixis/MusicFramework/Signal/Stateful/Transformer.hs
+++ Framework/Esferixis/MusicFramework/Signal/Stateful/Transformer.hs
@@ -11,13 +11,27 @@ import Data.Maybe
 
 import Esferixis.MusicFramework.Signal.Stateful.SignalChunk
 
-{- 
+{-
+   Operación de mutación del transformador
+-}
+data STTransformerMutationAction m sc = STTransformerMutationAction {
+     sftRemainingFramesToMutate :: Word64 -- Cantidad de frames para mutar
+   , sftDoMutationAction :: (Monad m, SFSignalChunk m sc ) => m ( SFTransformerSt m sc ) -- Operación de mutación al terminar de transformar la cantidad de frames especificada anteriormente
+   }
+
+{-
    Representación abstracta de un transformador stateful
    no manejado
+
+   Si hay acción de mutación debe efectuarse después de realizar el 'tick'
+   al acabar la cantidad de frames para mutar.
+   
+   Si la cantidad de frames para mutar es cero, tiene que realizarse el tick
+   y después realizar la mutación
 -}
 data SFTransformerSt m sc = UnmanagedProducerSt {
-     sftRemainingFrames :: Maybe Word64 -- Devuelve la cantidad de frames que quedan sin transformar, si es infinito devuelve Nothing
-   , sftTick :: (Monad m, SFSignalChunk m sc) => SignalChunkSection sc -> SignalChunkSection sc -> m ( SFTransformerSt m sc ) -- Realiza un 'tick' con la sección de chunk de entrada especificada, produciendo una sección de señal en la sección de chunk destino especificada, y devuelve el próximo estado
-   , sftTickInplace :: (Monad m, SFSignalChunk m sc) => SignalChunkSection sc -> m ( SFTransformerSt m sc ) -- Realiza un 'tick', produce una sección de señal en la sección chunk especificada, tomándola como entrada y como salida, y devuelve el próximo estado
+     sftMutationAction :: Maybe ( STTransformerMutationAction m sc ) -- Si el transformador muta, es la acción de mutación
+   , sftTick :: (Monad m, SFSignalChunk m sc) => SignalChunkSection sc -> SignalChunkSection sc -> m () -- Realiza un 'tick' con la sección de chunk de entrada especificada, produciendo una sección de señal en la sección de chunk destino especificada
+   , sftTickInplace :: (Monad m, SFSignalChunk m sc) => SignalChunkSection sc -> m ( SFTransformerSt m sc ) -- Realiza un 'tick', produce una sección de señal en la sección chunk especificada, tomándola como entrada y la sobreescribe con la salida
    , sftDelete :: (Monad m) => m () -- Destruye el transformador
    }
