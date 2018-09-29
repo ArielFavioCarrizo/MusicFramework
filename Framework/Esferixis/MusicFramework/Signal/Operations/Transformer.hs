5a7aac51f461f5850462544517b7583e3200dbbf
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-09-28 23:14:18 -0300

    Operacional: Futuro de operación anterior, en transformador

--- Framework/Esferixis/MusicFramework/Signal/Operations/Transformer.hs
+++ Framework/Esferixis/MusicFramework/Signal/Operations/Transformer.hs
@@ -35,17 +35,17 @@ data SFTransformer sc = SFTransformer { sftNewInstance :: AsyncIO ( Maybe ( SFTr
 data SFTransformerTickOp sc = SFTransformerPureTickOp ( SFSignalChunkIO sc ) | SFTransformerInplaceTickOp sc
 
 data SFTransformerSt sc = SFTransformerSt {
-     -- Máxima cantidad de frames con los que puede operar en el en el tick
+     -- Máxima cantidad de frames con los que puede operar en el tick
      sftMaxFrames :: Word64
      {-
-        Operación de transformado de frames con la operación especificada.
+        Operación de transformado de frames con la operación de tick, y el futuro de operación
+	anterior sobre el chunk especificado.
         Devuelve el futuro del resultado de la operación y el próximo estado.
 
         Si la transformación de señal termina devuelve Nothing
         y se destruye el transformador.
      -}
-   , sftTick :: (SFSignalChunk sc) => SFTransformerTickOp sc -> AsyncIO ( Future (), Maybe (SFTransformerSt sc) )
+   , sftTick :: (SFSignalChunk sc) => ( SFTransformerTickOp sc, Future () ) -> AsyncIO ( Future (), Maybe (SFTransformerSt sc) )
      -- Destruye el transformador
    , sftDelete :: AsyncIO ()
    }
-
