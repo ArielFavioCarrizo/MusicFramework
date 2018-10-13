163e9ce662db4c6757be2858b0a17d7093da7486
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-10-13 08:49:16 -0300

    I/O: Comienzo de reificado para simplificar diseño

--- Framework/Esferixis/MusicFramework/Signal/IO/MChunk.hs
+++ Framework/Esferixis/MusicFramework/Signal/IO/MChunk.hs
@@ -19,54 +19,20 @@ import Type.Reflection
 import Esferixis.Control.Concurrency.AsyncIO
 import Esferixis.Control.Concurrency.Promise
 import Esferixis.MusicFramework.Signal.Operations.Signal
+import Esferixis.Control.Concurrency.RefCounted
 
-data UnmanagedChunk ch = UnmanagedChunk {
-     ucChunk :: ch
-   , ucDelete :: AsyncIO ()
+data MChunk ch = MChunk {
+     mcChunk :: ch
+   , mcSplitRef :: AsyncIO ( ch, ch )
+   , mcTransform :: MChunk ch -> MChunk fc
+   , mcDeleteRef :: AsyncIO ()
    }
 
-{-
-   Recibe la entrada y una función que permite realizar un trabajo con el chunk.
-   Devuelve la salida de la operación.
--}
-type ConsumeFun ch s r = s -> (ch -> AsyncIO ()) -> AsyncIO r
-
-data MChunk ch r where
-   MkMChunk :: {
-         mcInput :: s
-       , mcMkChunk :: AsyncIO (UnmanagedChunk ch) 
-       , mcInitChunk :: ConsumeFun ch s r
-       } -> MChunk sc r
-   NextMChunk :: {
-         mcHead :: MChunk sc s
-       , mcCmd :: MChunkCmd sc s r
-       } -> MChunk sc r
-
-data MChunkCmd ch s r = ConsumeMChunkCmd ( ConsumeFun ch s r ) | BindMChunkCmd ( s -> AsyncIO r )
-
-mkMChunk :: s -> AsyncIO (UnmanagedChunk ch) -> ConsumeFun ch s r -> MChunk sc r
-mkMChunk input mkChunk initchunk =
-   MkMChunk {
-        mcInput = input
-      , mcMkChunk = mkChunk
-      , mcInitChunk = initchunk
-      }
-
-mChunkCmd :: MChunk ch s -> MChunkCmd ch s r -> MChunk ch r
-mChunkCmd srcMChunk cmd =
-   NextMChunk {
-        mcHead = srcMChunk
-      , mcCmd = cmd
-      }
-
--- Agrega un consumo al chunk
-mChunkConsume :: MChunk ch s -> ConsumeFun ch s r -> MChunk ch r
-mChunkConsume srcMChunk fun = mChunkCmd srcMChunk $ ConsumeMChunkCmd $ fun
-
--- Realiza un bind en el contexto del chunk
-mChunkBind :: MChunk ch s -> ( s -> AsyncIO r ) -> MChunk ch r
-mChunkBind srcMChunk fun = mChunkCmd srcMChunk $ BindMChunkCmd $ fun
-
--- TODO: Implementar
---mChunkRun :: MChunk sc r -> AsyncIO r
---mChunkRun mChunk =
+mkMChunk :: AsyncIO (SFFramesChunk fc) -> MChunk ch
+mkMChunk mkFramesChunk = do
+   framesChunk <- mkFramesChunk
+   rfFramesChunk < mkRefCounted framesChunk $ sffcDelete framesChunk
+   return $
+      MChunk (SFFramesChunk fc)
+           mcChunk = framesChunk
+         , mcSplitRef = 
