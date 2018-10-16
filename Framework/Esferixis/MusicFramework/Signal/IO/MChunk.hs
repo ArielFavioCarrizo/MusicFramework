03968150aa8d95c9ad504e0c668574841919e289
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-10-16 02:08:38 -0300

    I/O: Operaciones básicas de MChunk

--- Framework/Esferixis/MusicFramework/Signal/IO/MChunk.hs
+++ Framework/Esferixis/MusicFramework/Signal/IO/MChunk.hs
@@ -5,34 +5,48 @@
 {-# LANGUAGE GADTs #-}
 
 module Esferixis.MusicFramework.Signal.IO.MChunk(
-     UnmanagedChunk(ucChunk, ucDelete)
-   , MChunk
+     MChunk
    , mkMChunk
-   , mChunkConsume
-   , mChunkBind
+   , mcSplitRef
+   , mcConsume
    ) where
 
 import Data.Word
 import Data.Maybe
 import Data.Functor
-import Type.Reflection
+import Control.Monad.IO.Class
 import Esferixis.Control.Concurrency.AsyncIO
 import Esferixis.Control.Concurrency.Promise
 import Esferixis.MusicFramework.Signal.Operations.Signal
-import Esferixis.Control.Concurrency.RefCounted
+import Esferixis.MusicFramework.Signal.IO.FramesChunk
+import Esferixis.Control.Concurrency.RefCount
 
 data MChunk ch = MChunk {
      mcChunk :: ch
-   , mcSplitRef :: AsyncIO ( ch, ch )
-   , mcTransform :: MChunk ch -> MChunk fc
-   , mcDeleteRef :: AsyncIO ()
+   , mcRefCount :: RefCount
    }
 
-mkMChunk :: AsyncIO (SFFramesChunk fc) -> MChunk ch
+mkMChunk :: (SFFramesChunk fc) => IO fc -> AsyncIO (MChunk fc)
 mkMChunk mkFramesChunk = do
-   framesChunk <- mkFramesChunk
-   rfFramesChunk < mkRefCounted framesChunk $ sffcDelete framesChunk
-   return $
-      MChunk (SFFramesChunk fc)
-           mcChunk = framesChunk
-         , mcSplitRef = 
+   chunk <- liftIO $ mkFramesChunk
+   refCount <- mkRefCount $ liftIO $ sffcDelete chunk
+
+   return
+      MChunk {
+           mcChunk = chunk
+         , mcRefCount = refCount
+         }
+
+mcSplitRef :: MChunk ch -> AsyncIO ( MChunk ch, MChunk ch )
+mcSplitRef srcMChunk = do
+   refCountIncRef $ mcRefCount srcMChunk
+   return ( srcMChunk, srcMChunk )
+
+mcTransform :: MChunk sch -> ( sch -> dch ) -> MChunk dch
+mcTransform srcMChunk fun =
+   srcMChunk { mcChunk = fun $ mcChunk srcMChunk }
+
+mcConsume :: MChunk ch -> ( ch -> AsyncIO () ) -> AsyncIO ()
+mcConsume mchunk fun = do
+   fun $ mcChunk mchunk
+   refCountDecRef $ mcRefCount mchunk
