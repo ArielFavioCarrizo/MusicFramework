5cb0755027628cd9ed36b7b03428b7a99b8315d8
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-10-28 14:48:20 -0300

    SignalIO, MSignalIO: Consideración de variación de entorno, correcciones

--- Framework/Esferixis/MusicFramework/Signal/IO/MSignalIO.hs
+++ Framework/Esferixis/MusicFramework/Signal/IO/MSignalIO.hs
@@ -14,7 +14,7 @@ module Esferixis.MusicFramework.Signal.IO.MSignalIO(
         , MSIONewSymmetricTransformer
         , MSIOTransform
         , MSIONewBufferedTransformer
-        , MSIODispose
+        , MSIORemoveRef
         , MSIOBind
         , MSIOReturn
         , MSIOFail
@@ -35,86 +35,99 @@ module Esferixis.MusicFramework.Signal.IO.MSignalIO(
 
 import Data.Word
 import Data.Maybe
+import Data.Proxy
 import Control.Applicative
 import Control.Monad
 
 class MSIODisposable a
 
-class (MSIODisposable fc) => MFramesChunk fc where
-   fcLength :: fc -> Word64
-   fcChannels :: fc -> Word32
+class (MSIODisposable fc) => MFramesChunk e fc csc | e -> fc, fc -> csc where
+   fcLength :: Proxy e -> fc -> Word64
+   fcChannels :: Proxy e -> fc -> Word32
 
 class (MSIODisposable sc) => MSChunk sc where
    scLength :: sc -> Word64
 
-class (MSChunk lsc, MSChunk rsc, MSChunk tsc) => MSChunkTuple tsc lsc rsc | lsc -> tsc, rsc -> tsc
+class (MSChunk lsc, MSChunk rsc, MSChunk tsc) => MSChunkTuple tsc lsc rsc | lsc rsc -> tsc
 
 class (MSIODisposable p) => MSIOUnitGen p
 
-class (MSIOUnitGen p, MSChunk sc) => MSIOProducer p sc | p -> sc
+class (MSIOUnitGen p, MSChunk sc) => MSIOProducer e p sc | e sc -> p
 
-class (MSIOProducer p sc) => MSIOProducerTemplate pt p sc
+class (MSIOProducer e p sc) => MSIOProducerTemplate e pt p sc | e pt -> p sc
 
-class (MSIOUnitGen t, MSChunk isc, MSChunk osc) => MSIOSymmetricTransformer t isc osc | t -> isc, t -> osc
+class (MSIOUnitGen t, MSChunk isc, MSChunk osc) => MSIOSymmetricTransformer e t isc osc | isc osc -> t
 
-class (MSIOSymmetricTransformer t isc osc) => MSIOSymmetricTransformerTemplate tt t isc osc | tt -> t, tt -> isc, tt -> osc
+class (MSIOSymmetricTransformer e t isc osc) => MSIOSymmetricTransformerTemplate e tt t isc osc | e tt -> t isc osc
 
-data MSIOBufferedTransformer p c isc osc = MSIOBufferedTransformer {
-     msioInput :: (MSIOConsumer c isc) => p
-   , msioOutput :: (MSIOProducer p osc) => c
+data MSIOBufferedTransformer e c p isc osc = MSIOBufferedTransformer {
+     msioInput :: (MSIOConsumer e c isc) => c
+   , msioOutput :: (MSIOProducer e p osc) => p
    }
 
-class (MSIOConsumer c isc, MSIOProducer p osc, MSChunk isc, MSChunk osc) => MSIOBufferedTransformerTemplate tt p c isc osc
+class (MSIOConsumer e c isc, MSIOProducer e p osc, MSChunk isc, MSChunk osc) => MSIOBufferedTransformerTemplate e tt c p isc osc | e tt -> c p isc osc
 
-class (MSChunk osc) => MSIOConsumer c osc | c -> osc
+class (MSChunk osc) => MSIOConsumer e c osc | e osc -> c
 
-class (MSIOUnitGen c, MSIOConsumer c sc) => MSIOConsumerTemplate ct c sc
+class (MSIOUnitGen c, MSIOConsumer e c sc) => MSIOConsumerTemplate e ct c sc | e ct -> c sc
 
--- Acción con chunks de señales mutables
-data MSignalIO r where
+-- Acción con chunks de señales mutables, indicando el tipo del entorno, y el tipo de valor de retorno
+data MSignalIO e r where
    -- Dado el UnitGen devuelve la longitud máxima garantizada que puede procesar de chunks de ahora para adelante. Si devuelve cero significa que no puede procesar más.
-   MSIOMaxChunkLength :: (MSIOUnitGen ug) => ug -> MSignalIO Word64
+   MSIOMaxChunkLength :: (MSIOUnitGen ug) => ug -> MSignalIO e Word64
+
+   -- Crea un chunk de frames con la longitud y los canales especificados
+   MSIONewFramesChunk :: (MFramesChunk e fc csc) => Word64 -> Word32 -> MSignalIO e fc
+   -- Devuelve el chunk de señal del canal de chunk de frames especificado
+   MSIOChannel :: (MFramesChunk e fc csc) => fc -> Word32 -> MSignalIO e csc
+
+   -- Une dos chunks en una tupla
+   MSIOJoin :: (MSChunkTuple tsc lsc rsc) => lsc -> rsc -> MSignalIO e tsc
+   -- Devuelve una porción del chunk de señal con el offset y el tamaño especificados
+   MSIOSection :: (MSChunk sc) => sc -> Word64 -> Word64 -> MSignalIO e sc
+   -- Copia el contenido del primer chunk de señal al segundo. Ambos tienen que ser de la misma longitud.
+   MSIOCopy :: (MSChunk sc) => sc -> sc -> MSignalIO e ()
 
    -- Crea un productor con el template especificado
-   MSIONewProducer :: (MSIOProducerTemplate pt p sc) => pt -> MSignalIO p
+   MSIONewProducer :: (MSIOProducerTemplate e pt p sc) => pt -> MSignalIO e p
    -- Produce en el chunk especificado
-   MSIOPopChunk :: (MSIOProducer p sc) => p -> sc -> MSignalIO sc
+   MSIOPopChunk :: (MSIOProducer e p sc) => p -> sc -> MSignalIO e sc
 
    -- Crea un consumidor con el template especificado
-   MSIONewConsumer :: (MSIOConsumerTemplate ct c sc) => ct -> MSignalIO c
+   MSIONewConsumer :: (MSIOConsumerTemplate e ct c sc) => ct -> MSignalIO e c
    -- Empuja un chunk al consumidor especificado
-   MSIOPushChunk :: (MSIOConsumer c sc) => c -> sc -> MSignalIO ()
+   MSIOPushChunk :: (MSIOConsumer e c sc) => c -> sc -> MSignalIO e ()
 
    -- Crea un transformador simétrico con el template especificado
-   MSIONewSymmetricTransformer :: (MSIOSymmetricTransformerTemplate tt t isc osc) => tt -> MSignalIO t
+   MSIONewSymmetricTransformer :: (MSIOSymmetricTransformerTemplate e tt t isc osc) => tt -> MSignalIO e t
    -- Realiza una transformación con el transformador simétrico, el chunk de entrada y el chunk de salida especificados. Devuelve el chunk transformado.
-   MSIOTransform :: (MSIOSymmetricTransformer t isc osc) => t -> isc -> osc -> MSignalIO ()
+   MSIOTransform :: (MSIOSymmetricTransformer e t isc osc) => t -> isc -> osc -> MSignalIO e ()
 
    -- Crea un transformador buffereado con el template especificado
-   MSIONewBufferedTransformer :: (MSIOBufferedTransformerTemplate tt p c isc osc) => tt -> MSignalIO ( MSIOBufferedTransformer p c isc osc )
+   MSIONewBufferedTransformer :: (MSIOBufferedTransformerTemplate e tt c p isc osc) => tt -> MSignalIO e ( MSIOBufferedTransformer e c p isc osc )
 
-   -- Termina el uso del objeto especificado
-   MSIODispose :: (MSIODisposable a) => a -> MSignalIO ()
+   -- Remueve la referencia al objeto especificado. La invalida. Después no puede volver a usarse.
+   MSIORemoveRef :: (MSIODisposable a) => a -> MSignalIO e ()
 
    -- Bind monádico
-   MSIOBind :: MSignalIO s -> ( s -> MSignalIO r ) -> MSignalIO r
+   MSIOBind :: MSignalIO e s -> ( s -> MSignalIO e r ) -> MSignalIO e r
 
    -- Return monádico
-   MSIOReturn :: r -> MSignalIO r
+   MSIOReturn :: r -> MSignalIO e r
 
    -- Error monádico
-   MSIOFail :: String -> MSignalIO r
+   MSIOFail :: String -> MSignalIO e r
 
-instance Monad MSignalIO where
+instance Monad (MSignalIO e) where
    preSignalIO >>= k = MSIOBind preSignalIO k
 
    return value = MSIOReturn value
    
    fail message = MSIOFail message
 
-instance Applicative MSignalIO where
+instance Applicative (MSignalIO e) where
    pure = return
    (<*>) = ap
 
-instance Functor MSignalIO where
+instance Functor (MSignalIO e) where
    fmap = liftM
--- Framework/Esferixis/MusicFramework/Signal/IO/SignalIO.hs
+++ Framework/Esferixis/MusicFramework/Signal/IO/SignalIO.hs
@@ -17,7 +17,7 @@ module Esferixis.MusicFramework.Signal.IO.SignalIO(
         , SIOSplit
         , SIOSection
         , SIOFuse
-        , SIODispose
+        , SIORemoveRef
         , SIOBind
         , SIOReturn
         , SIOFail
@@ -36,6 +36,7 @@ module Esferixis.MusicFramework.Signal.IO.SignalIO(
 
 import Data.Word
 import Data.Maybe
+import Data.Proxy
 import Control.Applicative
 import Control.Monad
 
@@ -44,73 +45,73 @@ class SIODisposable a
 class (SIODisposable sc) => SChunk sc where
    scLength :: sc -> Word64
 
-class (SChunk lsc, SChunk rsc, SChunk tsc) => SChunkTuple tsc lsc rsc | lsc -> tsc, rsc -> tsc
+class (SChunk lsc, SChunk rsc, SChunk tsc) => SChunkTuple tsc lsc rsc | lsc rsc -> tsc
 
 class (SIODisposable p) => SIOUnitGen p
 
-class (SIOUnitGen p, SChunk sc) => SIOProducer p sc | p -> sc
+class (SIOUnitGen p, SChunk sc) => SIOProducer e p sc | e sc -> p
 
-class (SIOProducer p sc) => SIOProducerTemplate pt p sc
+class (SIOProducer e p sc) => SIOProducerTemplate e pt p sc | e pt -> p sc
 
-class (SIOUnitGen t, SChunk isc, SChunk osc) => SIOTransformer t isc osc | t -> isc, t -> osc
+class (SIOUnitGen t, SChunk isc, SChunk osc) => SIOTransformer e t isc osc | e isc osc -> t
 
-class (SIOTransformer t isc osc) => SIOTransformerTemplate tt t isc osc
+class (SIOTransformer e t isc osc) => SIOTransformerTemplate e tt t isc osc | e tt -> t isc osc
 
-class (SChunk osc) => SIOConsumer c osc | c -> osc
+class (SChunk osc) => SIOConsumer e c osc | e osc -> c
 
-class (SIOUnitGen c, SIOConsumer c sc) => SIOConsumerTemplate ct c sc
+class (SIOUnitGen c, SIOConsumer e c sc) => SIOConsumerTemplate e ct c sc | e ct -> c sc
 
--- Acción con señales
-data SignalIO r where
+-- Acción con señales, indicando el tipo del entorno, y el tipo de valor de retorno
+data SignalIO e r where
    -- Dado el UnitGen devuelve la longitud máxima garantizada que puede procesar de chunks de ahora para adelante. Si devuelve cero significa que no puede procesar más.
-   SIOMaxChunkLength :: (SIOUnitGen ug) => ug -> SignalIO Word64
+   SIOMaxChunkLength :: (SIOUnitGen ug) => ug -> SignalIO e Word64
 
    -- Crea un productor con el template especificado
-   SIONewProducer :: (SIOProducerTemplate pt p sc) => pt -> SignalIO p
+   SIONewProducer :: (SIOProducerTemplate e pt p sc) => pt -> SignalIO e p
    -- Devuelve un chunk con el productor y el tamaño de chunk deseado
-   SIOPopChunk :: (SIOProducer p sc) => p -> Word64 -> SignalIO sc
+   SIOPopChunk :: (SIOProducer e p sc) => p -> Word64 -> SignalIO e sc
 
    -- Crea un consumidor con el template especificado
-   SIONewConsumer :: (SIOConsumerTemplate ct c sc) => ct -> SignalIO c
+   SIONewConsumer :: (SIOConsumerTemplate e ct c sc) => ct -> SignalIO e c
    -- Empuja un chunk al consumidor especificado
-   SIOPushChunk :: (SIOConsumer c sc) => c -> sc -> SignalIO ()
+   SIOPushChunk :: (SIOConsumer e c sc) => c -> sc -> SignalIO e ()
 
    -- Crea un transformador con el template especificado
-   SIONewTransformer :: (SIOTransformerTemplate tt t isc osc) => tt -> SignalIO t
+   SIONewTransformer :: (SIOTransformerTemplate e tt t isc osc) => tt -> SignalIO e t
    -- Realiza una transformación con el transformador y el chunk de entrada especificado. Devuelve el chunk transformado.
-   SIOTransform :: (SIOTransformer t isc osc) => t -> isc -> SignalIO osc
+   SIOTransform :: (SIOTransformer e t isc osc) => t -> isc -> SignalIO e osc
 
    -- Une dos chunks en una tupla
-   SIOJoin :: (SChunkTuple tsc lsc rsc) => lsc -> rsc -> SignalIO tsc
+   SIOJoin :: (SChunkTuple tsc lsc rsc) => lsc -> rsc -> SignalIO e tsc
    -- Divide la tupla de chunks, separándola en sus componentes
-   SIOSplit :: (SChunkTuple tsc lsc rsc) => tsc -> SignalIO (lsc, rsc)
+   SIOSplit :: (SChunkTuple tsc lsc rsc) => tsc -> SignalIO e (lsc, rsc)
    -- Devuelve una porción del chunk con el offset y el tamaño especificados
-   SIOSection :: (SChunk sc) => sc -> Word64 -> Word64 -> SignalIO sc
+   SIOSection :: (SChunk sc) => sc -> Word64 -> Word64 -> SignalIO e sc
    -- Fusiona dos chunks. Del izquierdo al derecho.
-   SIOFuse :: (SChunk sc) => sc -> sc -> SignalIO sc
+   SIOFuse :: (SChunk sc) => sc -> sc -> SignalIO e sc
 
-   -- Termina el uso del objeto especificado
-   SIODispose :: (SIODisposable a) => a -> SignalIO ()
+   -- Remueve la referencia al objeto especificado. La invalida. Después no puede volver a usarse.
+   SIORemoveRef :: (SIODisposable a) => a -> SignalIO e ()
 
    -- Bind monádico
-   SIOBind :: SignalIO s -> ( s -> SignalIO r ) -> SignalIO r
+   SIOBind :: SignalIO e s -> ( s -> SignalIO e r ) -> SignalIO e r
 
    -- Return monádico
-   SIOReturn :: r -> SignalIO r
+   SIOReturn :: r -> SignalIO e r
 
    -- Error monádico
-   SIOFail :: String -> SignalIO r
+   SIOFail :: String -> SignalIO e r
 
-instance Monad SignalIO where
+instance Monad (SignalIO e) where
    preSignalIO >>= k = SIOBind preSignalIO k
 
    return value = SIOReturn value
    
    fail message = SIOFail message
 
-instance Applicative SignalIO where
+instance Applicative (SignalIO e) where
    pure = return
    (<*>) = ap
 
-instance Functor SignalIO where
+instance Functor (SignalIO e) where
    fmap = liftM
