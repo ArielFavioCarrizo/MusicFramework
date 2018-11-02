c0a054fd9482970c5021386c62675d17968e9b64
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-11-02 01:56:23 -0300

    SafeT: Correcciones

--- Framework/Esferixis/Control/Monad/SafeT.hs
+++ Framework/Esferixis/Control/Monad/SafeT.hs
@@ -6,8 +6,6 @@
 
 module Esferixis.Control.Monad.SafeT(
      SafeT
-   , SafeTResult
-   , mkSafeTResult
    , runSafeT
    ) where
 
@@ -19,9 +17,6 @@ import Control.Monad.Trans.Class
 data SafeT e m a where
    LiftedSafeT :: m a -> SafeT e m a
    BindSafeT :: SafeT e m b -> ( b -> SafeT e m a ) -> SafeT e m a
-
--- Resultado dentro de SafeT
-data SafeTResult e a = SafeTResult a
 
 instance (Monad m) => Monad (SafeT e m) where
    preSafeT >>= k = BindSafeT preSafeT k
@@ -40,33 +35,17 @@ instance (Monad m) => Functor (SafeT e m) where
 instance MonadTrans (SafeT e) where
    lift unlifted = LiftedSafeT unlifted
 
-{-
-   Crea un resultado dentro de la mónada SafeT
--}
-mkSafeTResult :: (Monad m) => a -> SafeT e m ( SafeTResult e a )
-mkSafeTResult value = return $ SafeTResult value
+data SafeTToken = SafeTToken
 
 {-
    Ejecute la acción que devuelve un resultado, garantizando
    que la descripción de efectos envueltos bajo SafeT no puedan
    ser manipulados, y que no puedan
    escapar de la mónada.
-
-   Lo hace de la manera siguiente:
-   
-   * La única función que puede construir el resultado no produce
-   otro efecto que producir el resultado a partir
-   de un valor.
-   Y el resultado sólo puede ser extraído por ésta función.
-   
-   * Todo valor cuyo tipo que incluya el 'phantom type' e
-   de entorno, no puede ser manipulado con las funciones marcadas con 'e'
-   fuera de la mónada.
 -}
-runSafeT :: (Monad m) => (forall e. SafeT e m ( SafeTResult e a ) ) -> m a
-runSafeT safeTAction = do
-   SafeTResult value <- runSafeTInternal safeTAction
-   return value
+runSafeT :: (Monad m) => ( forall e. SafeT e m a ) -> m a
+runSafeT safeTAction =
+   runSafeTInternal safeTAction
 
 runSafeTInternal :: (Monad m) => SafeT e m a -> m a
 runSafeTInternal (LiftedSafeT unlifted) = unlifted
